61f5a0f02e833f1f5ce093f4b3dd9817c529b969
atinepro <atinder@wizni.com>
2016-06-03 01:32:43 +0530

    Fixed datetimepicker directive to initialize it properly with ng-model value

--- zeppelin-web/src/components/form/datetimepicker.directive.js
+++ zeppelin-web/src/components/form/datetimepicker.directive.js
@@ -1,61 +1,68 @@
 'use strict';
 
 angular.module('zeppelinWebApp')
-    .provider('datetimepicker', function () {
-        var default_options = {
-          useCurrent: false,
-          format: 'MM/DD/YYYY hh:mm:ss A'
-        };
+  .provider('datetimepicker', function () {
+    var default_options = {
+      useCurrent: false,
+      format: 'MM/DD/YYYY hh:mm:ss A'
+    };
 
-        this.setOptions = function (options) {
-            default_options = options;
-        };
+    this.setOptions = function (options) {
+      default_options = options;
+    };
 
-        this.$get = function () {
-            return {
-                getOptions: function () {
-                    return default_options;
-                }
+    this.$get = function () {
+      return {
+        getOptions: function () {
+          return default_options;
+        }
+      };
+    };
+  })
+  .directive('datetimepicker', ['$timeout', 'datetimepicker', function ($timeout, datetimepicker) {
+    var default_options = datetimepicker.getOptions();
+    return {
+      require: '?ngModel',
+      restrict: 'A',
+      scope: {
+        options: '@'
+      },
+      link: function ($scope, $element, $attrs, ngModelCtrl) {
+        var passed_in_options = $scope.$eval($attrs.options);
+        var options = jQuery.extend({}, default_options, passed_in_options);
+
+        $element.on('dp.change', function (e) {
+          if (ngModelCtrl) {
+            $timeout(function () {
+              ngModelCtrl.$setViewValue(new Date(e.target.value));
+            });
+          }
+        });
+
+        var unregister = $scope.$watch(function () {
+          return ngModelCtrl.$modelValue;
+        }, initialize);
+
+        function initialize(value) {
+          ngModelCtrl.$setViewValue(value);
+          $element.datetimepicker(options);
+
+          if (ngModelCtrl) {
+            ngModelCtrl.$render = function () {
+              setPickerValue();
             };
-        };
-    })
-    .directive('datetimepicker', ['$timeout', 'datetimepicker', function ($timeout, datetimepicker) {
-        var default_options = datetimepicker.getOptions();
-        return {
-            require: '?ngModel',
-            restrict: 'A',
-            scope: {
-                options: '@'
-            },
-            link: function ($scope, $element, $attrs, ngModelCtrl) {
-                var passed_in_options = $scope.$eval($attrs.options);
-                var options = jQuery.extend({}, default_options, passed_in_options);
-                $element.on('dp.change', function (e) {
-                        if (ngModelCtrl) {
-                            $timeout(function () {
-                                ngModelCtrl.$setViewValue(new Date(e.target.value));
-                            });
-                        }
-                    })
-                    .datetimepicker(options);
+          }
+          setPickerValue();
+          unregister();
+        }
 
-                function setPickerValue() {
-                    var date = options.defaultDate || null;
-
-                    if (ngModelCtrl && ngModelCtrl.$viewValue) {
-                        date = ngModelCtrl.$viewValue;
-                    }
-
-                    $element.data('DateTimePicker').date(date);
-                }
-
-                if (ngModelCtrl) {
-                    ngModelCtrl.$render = function () {
-                        setPickerValue();
-                    };
-                }
-
-                setPickerValue();
-            }
-        };
-    }]);
+        function setPickerValue() {
+          var date = options.defaultDate || null;
+          if (ngModelCtrl && ngModelCtrl.$viewValue) {
+            date = ngModelCtrl.$viewValue;
+          }
+          $element.data('DateTimePicker').date(date && new Date(date));
+        }
+      }
+    };
+  }]);
