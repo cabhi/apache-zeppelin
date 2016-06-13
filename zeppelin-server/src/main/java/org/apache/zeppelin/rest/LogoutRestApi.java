5fcf1eebe4e9f1ade630aa25c5120a83e4bc33eb
Lavakush <lavakush@wizni.com>
2016-06-13 18:07:34 +0530

    ANALYTICS-71: Implemented logout functionality

--- zeppelin-server/src/main/java/org/apache/zeppelin/rest/LogoutRestApi.java
+++ zeppelin-server/src/main/java/org/apache/zeppelin/rest/LogoutRestApi.java
@@ -3,6 +3,7 @@ package org.apache.zeppelin.rest;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -17,21 +18,30 @@ import org.slf4j.LoggerFactory;
 @Produces("application/json")
 public class LogoutRestApi {
 private static final Logger LOG = LoggerFactory.getLogger(LogoutRestApi.class);
-	  @POST
-	  public Response logout() {
-	    JsonResponse response;
-	    
-	    Subject currentUser = org.apache.shiro.SecurityUtils.getSubject();
-	    currentUser.logout();
 
-	    Map<String, String> data = new HashMap<>();
-	    data.put("principal", "anonymous");
-	    data.put("roles", "");
-	    data.put("ticket", "anonymous");
-	   
-	    response = new JsonResponse(Response.Status.OK, "", data);
-	    LOG.warn(response.toString());
-	    return response.build();
-	  }
+/**
+ * Required by Swagger.
+ */
+public LogoutRestApi() {
+ super();
+}
+
+
+@POST
+public Response logout() {
+  JsonResponse response;
+  
+  Subject currentUser = org.apache.shiro.SecurityUtils.getSubject();
+  currentUser.logout();
+
+  Map<String, String> data = new HashMap<>();
+  data.put("principal", "anonymous");
+  data.put("roles", "");
+  data.put("ticket", "anonymous");
+ 
+  response = new JsonResponse(Response.Status.OK, "", data);
+  LOG.warn(response.toString());
+  return response.build();
+}
 
 }
